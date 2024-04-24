61da6df6912bb21262990488ec9ebaabcf9f8535
Pavlo-Oryshchych <oryshchychp@gmail.com>
2024-04-24 13:16:29 +0200

    Security almost finished

--- qr/src/main/java/com/oryshchych/qr/config/ApplicationConfig.java
+++ qr/src/main/java/com/oryshchych/qr/config/ApplicationConfig.java
@@ -1,9 +1,11 @@
 package com.oryshchych.qr.config;
 
+import com.oryshchych.qr.auditing.ApplicationAuditAware;
 import com.oryshchych.qr.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -17,11 +19,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RequiredArgsConstructor
 public class ApplicationConfig {
 
-    private final UserRepository userRepository;
+    private final UserRepository repository;
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return username -> userRepository.findUserByEmail(username)
+        return username -> repository.findUserByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 
@@ -34,13 +36,18 @@ public class ApplicationConfig {
     }
 
     @Bean
+    public AuditorAware<Long> auditorAware() {
+        return new ApplicationAuditAware();
+    }
+
+    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
-
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
